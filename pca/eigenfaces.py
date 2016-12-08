b89ecce2144b2889c9ed1f550b4063d1426b7c83
Arnaldo Gualberto <arnaldo.g12@gmail.com>
2016-12-08 17:03:02 -0300

    PCA

--- pca/eigenfaces.py
+++ pca/eigenfaces.py
@@ -14,8 +14,6 @@ The dataset used in this example is a preprocessed excerpt of the
 
 """
 
-
-
 print __doc__
 
 from time import time
@@ -33,7 +31,6 @@ from sklearn.svm import SVC
 
 # Display progress logs on stdout
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
-
 
 ###############################################################################
 # Download the data, if not already on disk and load it as numpy arrays
@@ -58,7 +55,6 @@ print "n_samples: %d" % n_samples
 print "n_features: %d" % n_features
 print "n_classes: %d" % n_classes
 
-
 ###############################################################################
 # Split into a training and testing set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
@@ -66,7 +62,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random
 ###############################################################################
 # Compute a PCA (eigenfaces) on the face dataset (treated as unlabeled
 # dataset): unsupervised feature extraction / dimensionality reduction
-n_components = 150
+n_components = 250
 
 print "Extracting the top %d eigenfaces from %d faces" % (n_components, X_train.shape[0])
 t0 = time()
@@ -81,6 +77,7 @@ X_train_pca = pca.transform(X_train)
 X_test_pca = pca.transform(X_test)
 print "done in %0.3fs" % (time() - t0)
 
+print(pca.explained_variance_ratio_)
 
 ###############################################################################
 # Train a SVM classification model
