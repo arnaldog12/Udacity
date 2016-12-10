3b1b81dc63b3351a4c1cd0d7b4b0370629a5360e
Arnaldo Gualberto <arnaldo.g12@gmail.com>
2016-12-10 08:16:17 -0300

    Validation

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -25,8 +25,18 @@ features_list = ["poi", "salary"]
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
-
-
 ### it's all yours from here forward!  
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.cross_validation import train_test_split
 
+clf = DecisionTreeClassifier()
+clf.fit(features, labels)
+pred = clf.predict(features)
+print(accuracy_score(pred, labels))
+
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=42)
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+print(accuracy_score(pred, labels_test))
 
